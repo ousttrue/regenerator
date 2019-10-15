32e78539361b17a661d38f3a63eb235810a16375
ousttrue <ousttrue@gmail.com>
2019-10-15 18:35:50 +0900

    sample code.
    from https://bastian.rieck.ru/blog/posts/2015/baby_steps_libclang_ast/

--- main.cpp
+++ main.cpp
@@ -1,4 +1,69 @@
-int main()
+#include <clang-c/Index.h>
+
+#include <iostream>
+#include <string>
+
+std::string getCursorKindName(CXCursorKind cursorKind)
 {
+    CXString kindName = clang_getCursorKindSpelling(cursorKind);
+    std::string result = clang_getCString(kindName);
+
+    clang_disposeString(kindName);
+    return result;
+}
+
+std::string getCursorSpelling(CXCursor cursor)
+{
+    CXString cursorSpelling = clang_getCursorSpelling(cursor);
+    std::string result = clang_getCString(cursorSpelling);
+
+    clang_disposeString(cursorSpelling);
+    return result;
+}
+
+CXChildVisitResult visitor(CXCursor cursor, CXCursor /* parent */, CXClientData clientData)
+{
+    CXSourceLocation location = clang_getCursorLocation(cursor);
+    if (clang_Location_isFromMainFile(location) == 0)
+        return CXChildVisit_Continue;
+
+    CXCursorKind cursorKind = clang_getCursorKind(cursor);
+
+    unsigned int curLevel = *(reinterpret_cast<unsigned int *>(clientData));
+    unsigned int nextLevel = curLevel + 1;
+
+    std::cout << std::string(curLevel, '-') << " " << getCursorKindName(cursorKind) << " (" << getCursorSpelling(cursor) << ")\n";
+
+    clang_visitChildren(cursor,
+                        visitor,
+                        &nextLevel);
+
+    return CXChildVisit_Continue;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return -1;
+
+    CXIndex index = clang_createIndex(0, 1);
+    const char *params[] = {
+        "-x",
+        "c++",
+    };
+    CXTranslationUnit tu = clang_createTranslationUnitFromSourceFile(index, argv[1], 2, params, 0, nullptr);
+
+    if (!tu)
+        return -1;
+
+    CXCursor rootCursor = clang_getTranslationUnitCursor(tu);
+
+    unsigned int treeLevel = 0;
+
+    clang_visitChildren(rootCursor, visitor, &treeLevel);
+
+    clang_disposeTranslationUnit(tu);
+    clang_disposeIndex(index);
+
     return 0;
 }
