1389d807700a3af9f761e10b93e62d2c3b2bed52
ousttrue <ousttrue@gmail.com>
2019-10-16 22:06:11 +0900

    cleanup

--- source/app.d
+++ source/app.d
@@ -2,39 +2,6 @@ import std.stdio;
 import std.conv;
 import libclang;
 
-int main(string[] args)
-{
-	if (args.length < 2)
-	{
-		return 1;
-	}
-
-	auto index = clang_createIndex(0, 1);
-
-	auto params = [
-		cast(byte*) "-x".ptr, cast(byte*) "c++".ptr,
-		cast(byte*) "-IC:/Program Files/LLVM/include".ptr
-	];
-	auto tu = clang_createTranslationUnitFromSourceFile(index,
-			cast(byte*) "C:/Program Files/LLVM/include/clang-c/Index.h".ptr, cast(int) params.length, params.ptr, 0, null);
-
-	if (!tu)
-	{
-		return 2;
-	}
-
-	auto rootCursor = clang_getTranslationUnitCursor(tu);
-
-	uint treeLevel = 0;
-
-	clang_visitChildren(rootCursor, &visitor, &treeLevel);
-
-	clang_disposeTranslationUnit(tu);
-	clang_disposeIndex(index);
-
-	return 0;
-}
-
 string getCursorKindName(CXCursorKind cursorKind)
 {
 	auto kindName = clang_getCursorKindSpelling(cursorKind);
@@ -53,25 +20,48 @@ string getCursorSpelling(CXCursor cursor)
 	return to!string(cast(immutable char*) result);
 }
 
-extern (C) CXChildVisitResult visitor(CXCursor cursor, CXCursor /* parent */ , void* clientData)
+extern (C) CXChildVisitResult visitor(CXCursor cursor, CXCursor /* parent */ ,
+		immutable char* indent)
 {
-	auto location = clang_getCursorLocation(cursor);
-	if (clang_Location_isFromMainFile(location) == 0)
-		return CXChildVisitResult.CXChildVisit_Continue;
-
-	auto cursorKind = clang_getCursorKind(cursor);
-
-	uint curLevel = *(cast(uint*) clientData);
-	uint nextLevel = curLevel + 1;
-
-	// indent
-	for (int i = 0; i < curLevel; ++i)
+	if (clang_Location_isFromMainFile(clang_getCursorLocation(cursor)))
 	{
-		write("  ");
+		auto cursorKind = clang_getCursorKind(cursor);
+
+		writefln("%s%s (%s)", to!string(indent),
+				getCursorKindName(cursorKind), getCursorSpelling(cursor));
+
+		clang_visitChildren(cursor, &visitor, cast(void*)(to!string(indent) ~ "  ").ptr);
 	}
-	writefln("%s (%s)", getCursorKindName(cursorKind), getCursorSpelling(cursor));
-
-	clang_visitChildren(cursor, &visitor, &nextLevel);
-
 	return CXChildVisitResult.CXChildVisit_Continue;
+}
+
+int main(string[] args)
+{
+	if (args.length < 2)
+	{
+		return 1;
+	}
+
+	auto index = clang_createIndex(0, 1);
+
+	auto params = [
+		cast(byte*) "-x".ptr, cast(byte*) "c++".ptr,
+		cast(byte*) "-IC:/Program Files/LLVM/include".ptr
+	];
+	auto tu = clang_createTranslationUnitFromSourceFile(index,
+			cast(byte*) "C:/Program Files/LLVM/include/clang-c/Index.h".ptr,
+			cast(int) params.length, params.ptr, 0, null);
+	if (!tu)
+	{
+		return 2;
+	}
+
+	auto rootCursor = clang_getTranslationUnitCursor(tu);
+
+	clang_visitChildren(rootCursor, &visitor, cast(void*) "".ptr);
+
+	clang_disposeTranslationUnit(tu);
+	clang_disposeIndex(index);
+
+	return 0;
 }
