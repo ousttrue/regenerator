e6fb2a5f3853ff61e9159fdd90a4ce58fbaf400e
ousttrue <ousttrue@gmail.com>
2020-06-11 19:41:25 +0900

    fix include

--- lua/cs_libclang.lua
+++ lua/cs_libclang.lua
@@ -4,9 +4,13 @@ local CS = require 'csharp'
 ------------------------------------------------------------------------------
 -- command line
 ------------------------------------------------------------------------------
-local src, dir = table.unpack {...}
+local src, dir = table.unpack {
+    ...
+}
 
-print_table({...})
+print_table({
+    ...
+})
 local USAGE = 'regenerator.exe cs_libclang.lua {LLVM_DIR} {cs_dst_dir}'
 if not dir then
     error(USAGE)
@@ -15,13 +19,22 @@ end
 ------------------------------------------------------------------------------
 -- libclang CIndex
 ------------------------------------------------------------------------------
-local headers = {"clang-c/Index.h", "clang-c/CXString.h"}
+local headers = {
+    "clang-c/Index.h",
+    "clang-c/CXString.h"
+}
 for i, f in ipairs(headers) do
     local header = string.format('%s/%s', src, f)
     print(header)
     headers[i] = header
 end
-local sourceMap = ClangParse {headers = headers, defines = {}}
+local sourceMap = ClangParse {
+    includes = {
+        src
+    },
+    headers = headers,
+    defines = {}
+}
 if not sourceMap then
     error('no sourceMap')
 end
@@ -37,7 +50,7 @@ local option = {
     overload = {},
     dll_map = {
         index = "libclang",
-        cxstring = "libclang",
+        cxstring = "libclang"
     }
 }
 
